63ec175d3465a050a59f7f0133893aefe001d3c0
Nicholas Heneisen <nick@heneisen.com>
2017-04-29 08:06:11 -0400

    Completed most functionality. Having trouble figuring out how to post the information to the modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,13 +9,26 @@ module.exports = function(app) {
     });
 //Create a POST route with /api/friends/ to handle incoming survey results and compatibiity logic.
     app.post("/api/friends", function(req, res) {
-        console.log(req.body);
-        console.log(friends[0]);
-        var estFriend = "";
-        var bestFriendScore = ;
+        var bestFriend = "";
+        var bestFriendImg = "";
+        var bestFriendScore = 900;
         for (friend=0;friend<friends.length;friend++) {
-            
+            score = 0
+            console.log(friends[friend].Name);
+            for (q=0; q<friends[friend].Answers.length; q++) {
+                score += Math.abs(req.body.Answers[q] - friends[friend].Answers[q])
+            }
+            if (score<bestFriendScore) {
+                bestFriendScore = score;
+                bestFriend = friends[friend].Name;
+                bestFriendImg = friends[friend].Image;
+            }
+            console.log(score);
         }
+        friends.push(req.body);
+        console.log(bestFriend);
+        console.log(bestFriendImg);
+        console.log(bestFriendScore);
     });
 
 
